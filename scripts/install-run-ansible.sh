9dec4966edfae4558e4720277f176f275f17071d
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2024-03-09 06:18:25 -0500

    remove_ansible_duplication

---
+++ scripts/install-run-ansible.sh
@@ -0,0 +1,13 @@
+cat <<EOF >> harden-ubi8.yml
+---
+- hosts: all
+  roles:
+     - ansible-role-rhel8-stig
+EOF
+
+python3.11 -m venv ansibletemp
+source ansibletemp/bin/activate \
+    && python3 -m pip install --upgrade pip \
+    && python3 -m pip install ansible ansible-core \
+    && git clone https://github.com/RedHatOfficial/ansible-role-rhel8-stig.git \
+    && ansible-playbook -i "localhost," -c local harden-ubi8.yml --skip-tags="sudo_remove_no_authenticate,sudo_remove_nopasswd,sudoers_default_includedir,sudo_require_reauthentication,sudoers_validate_passwd,package_rng-tools_installed,enable_authselect,DISA-STIG-RHEL-08-040110"
