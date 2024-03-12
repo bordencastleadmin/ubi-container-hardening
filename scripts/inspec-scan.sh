41ca39e6f91516b98f5716a4f16a85496a7f7fcd
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2024-03-12 19:30:03 -0400

    Update inspec-scan.sh

--- scripts/inspec-scan.sh
+++ scripts/inspec-scan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 containerName=$1
+profile=$2
 
 docker stop $(docker ps -a -q)
 docker remove $(docker ps -a -q)
@@ -8,4 +9,4 @@ docker container run --detach -i --name ${containerName} ${containerName}
 
 containerId=$(docker container ls --all | grep -w ${containerName} | awk '{print $1}')
 
-inspec exec ${containerName} -t docker://${containerId}
+inspec exec ${profile} -t docker://${containerId}
