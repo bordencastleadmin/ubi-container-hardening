f7e8c89e15c53addcc1fe854168558ad5dd46481
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2024-03-12 19:37:40 -0400

    Update inspec-scan.sh

--- scripts/inspec-scan.sh
+++ scripts/inspec-scan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 containerName=$1
-profile=$2
 
 curl --tlsv1.3 -sSf --proto "=https" -L https://omnitruck.cinc.sh/install.sh | bash -s -- -v 18
 
@@ -11,4 +10,4 @@ docker container run --detach -i --name ${containerName} ${containerName}
 
 containerId=$(docker container ls --all | grep -w ${containerName} | awk '{print $1}')
 
-inspec exec ${profile} -t docker://${containerId} --input-file inspec-inputs.yml --tags container container-conditional --reporter junit2:/tmp/junit.xml html:www/index.html
+inspec exec https://github.com/mitre/redhat-enterprise-linux-8-stig-baseline/archive/refs/tags/v1.12.0.tar.gz -t docker://${containerId} --input-file inspec-inputs.yml --tags container container-conditional --reporter junit2:/tmp/junit.xml html:www/index.html
