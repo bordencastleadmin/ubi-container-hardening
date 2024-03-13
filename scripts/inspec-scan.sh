3b846578f97c152b86b36c5f0be230cf37b9b43c
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2024-03-13 07:08:18 -0400

    Update inspec-scan.sh

--- scripts/inspec-scan.sh
+++ scripts/inspec-scan.sh
@@ -11,6 +11,7 @@ docker run -itd --name ${name} ${tag}
 containerId=$(docker container ls --all | grep -w ${name} | awk '{print $1}')
 
 inspec plugin search inspec- | grep -i docker
+inspec pkugin search train- | grep -i docker
 inspec plugin install train-docker
 inspec plugin install inspec-docker
 inspec exec https://github.com/mitre/redhat-enterprise-linux-8-stig-baseline/archive/refs/tags/v1.12.0.tar.gz -t docker://${containerId} --input-file inspec-inputs.yml --tags container container-conditional --reporter junit2:/tmp/junit.xml html:www/index.html
