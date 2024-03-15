020b806ddc18c30ed1b2df4f2c912b79cf8c0ae1
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2024-03-15 06:53:32 -0400

    Update cleanup-dnf.sh

--- scripts/cleanup-dnf.sh
+++ scripts/cleanup-dnf.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 dnf remove git scc* python3.11-pip unzip -y \
-  && rm -rf ${BUILDER} 
+  && rm -rf ${BUILDER} && \ 
+  chmod 0755 /usr/bin/launch && \
+  chmod 0755 /usr/bin/logging.sh && \
+  chmod 0755 /bin/launch && \
+  chmod 0755 /bin/logging.sh
