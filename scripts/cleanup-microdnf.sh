924199b84b78371436159f5851ca5fa12430a6d1
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2024-03-15 06:56:36 -0400

    Update cleanup-microdnf.sh

--- scripts/cleanup-microdnf.sh
+++ scripts/cleanup-microdnf.sh
@@ -7,5 +7,6 @@ dnf remove python3.11-pip git scc* unzip -y \
   chmod 0755 /usr/bin/launch && \
   chmod 0755 /usr/bin/logging.sh && \
   chmod 0755 /bin/launch && \
-  chmod 0755 /bin/logging.sh
-  
+  chmod 0755 /bin/logging.sh && \
+  chown -R root:root /lib/.build-id && \
+  chown -R root:root /usr/lib/.build-id
