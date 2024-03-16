d20b79c815ab57f09e5563eae99d3e75bb57659a
BordenIT, LLC Admin <49888529+bordenit@users.noreply.github.com>
2024-03-15 21:11:42 -0400

    Update cleanup-microdnf.sh

--- scripts/cleanup-microdnf.sh
+++ scripts/cleanup-microdnf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-dnf remove python3.11-pip git scc* unzip -y \
-  && rm -vf /etc/dnf/protected.d/dnf.conf \
-  && rpm --nodeps -e dnf \
-  && microdnf clean all -y \
-  && rm -rf ${BUILDER} && \
+dnf remove python3.11-pip git scc* unzip -y && \
+  rm -vf /etc/dnf/protected.d/dnf.conf && \
+  rpm --nodeps -e dnf  && \
+  microdnf clean all -y && \
+  rm -rf ${BUILDER} && \
   chmod 0755 /usr/bin/launch && \
   chmod 0755 /usr/bin/logging.sh && \
   chmod 0755 /bin/launch && \
