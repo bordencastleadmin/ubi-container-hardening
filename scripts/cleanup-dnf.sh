b56a24ba14300ac2f44ad0526d87f2054b9f90f3
thermic <blakefrain@live.com>
2025-07-22 08:41:59 -0400

    uninstalling ansible

--- scripts/cleanup-dnf.sh
+++ scripts/cleanup-dnf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-dnf remove git scc* python3.11-pip unzip -y && \
+python3 -m pip uninstall ansible ansible-core && \
+  dnf remove git scc* python3.11-pip unzip -y && \
   rm -rf ${BUILDER} && \
   chown -R root:root /lib/.build-id && \
   chown -R root:root /usr/lib/.build-id
