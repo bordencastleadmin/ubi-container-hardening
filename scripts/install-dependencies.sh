fb8d2e91fa6ee10c4ce47d9b579d335eb5f2876c
blakefrain <48569400+blakefrain@users.noreply.github.com>
2025-07-25 20:04:08 -0400

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -16,7 +16,7 @@ EOF
 python3.11 -m venv ansibletemp
 source ansibletemp/bin/activate \
     && python3 -m pip install --upgrade pip \
-    && python3 -m pip install ansible ansible-core \
+    && python3 -m pip install ansible "ansible-core==2.11.*" \
     && git clone https://github.com/RedHatOfficial/ansible-role-rhel8-stig.git \
     && ansible-playbook -i "localhost," -c local harden-ubi8.yml --skip-tags="sudo_remove_no_authenticate,sudo_remove_nopasswd,sudoers_default_includedir,sudo_require_reauthentication,sudoers_validate_passwd,package_rng-tools_installed,enable_authselect,DISA-STIG-RHEL-08-040110"
 
